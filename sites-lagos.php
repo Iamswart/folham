62571c97d8032c9223dfaca36884b05118d50cea
Iamswart <swartjide@gmail.com>
2022-11-22 02:51:11 +0100

    make it better

--- sites-lagos.php
+++ sites-lagos.php
@@ -61,7 +61,7 @@
 										<div class="modal-content">
 											<div class="modal-header">
 												<h4 class="modal-title" id="myModalLabel">Eko hotel road </h4>
-												<button type="button" class="close" data-dismiss="modal" >&times;</button>
+												<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
 											</div>
 											<div class="modal-body">
 												<div class="sitemodal" style="display: flex; height: 420px">
